a95b36b054fd6e2d732ec8ab48f671c59eab5f0c
ach-ber <77842805+ach-ber@users.noreply.github.com>
2022-03-26 14:59:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 4000;
+const port = 4000;
 app.listen(port);
 
