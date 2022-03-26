0fdf27fb8d4d36d839c2a62848d98ec266a7b1c6
ach-ber <77842805+ach-ber@users.noreply.github.com>
2022-03-26 15:40:23 +0100

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-})
+});
+
+// rajouté deb
+const path = require("path");
+
+module.exports = {
+ outputDir: path.resolve(__dirname, "../public")
+}
+
+// rajouté fin
